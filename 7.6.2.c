bca251deba4997ee3a18be7d85aec0d816f3f7b5
vcup <vcupmoe@gmail.com>
2020-12-21 16:36:16 +0800

    改进7.6.2.c，使输入输出符合直觉

--- 7.6.2.c
+++ 7.6.2.c
@@ -2,15 +2,18 @@
 
 int main(void) {
     char ch;
-    int cont;
+    int cont = 0;
     printf("input some char:");
-    while ((ch = getchar()) != '#') {
-        if (ch == '\n')
+    while (ch != '#') {
+        if ((ch = getchar()) == '\n') {
+            cont = 0;
             continue;
+        }
         printf("'%c'-%-4d", ch, ch);
         if ((++cont % 8) == 0)
             putc('\n', stdout);
     }
-    putc('\n', stdout);
+    if ((cont % 8) != 0)
+        putc('\n', stdout);
     return 0;
 }
